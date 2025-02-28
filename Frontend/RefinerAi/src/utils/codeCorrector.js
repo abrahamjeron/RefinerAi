cbfd5eae4006d316199a7d66dd15e11bb4ece4f8
abrahamjeron <abrahamjeron40@gmail.com>
2025-02-28 14:54:02 +0530

    did the code passing to the gemini

--- Frontend/RefinerAi/src/utils/codeCorrector.js
+++ Frontend/RefinerAi/src/utils/codeCorrector.js
@@ -1,4 +1,3 @@
-// src/utils/codeCorrector.js
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { JsonOutputParser } from "@langchain/core/output_parsers";
@@ -7,24 +6,22 @@ import dotenv from "dotenv";
 // Load environment variables from .env file
 // dotenv.config();
 
-const code = "import math prit('heeelo workds')"; // Code to be corrected
-const changes = "correct the syntax";
-
 const model = new ChatGoogleGenerativeAI({
     modelName: "gemini-1.5-flash-latest",
-    apiKey: "AIzaSyBlY6dGU0u7iBlHle52xkat64VrNKKldL8", // Use env variable for security
+    apiKey: "AIzaSyBlY6dGU0u7iBlHle52xkat64VrNKKldL8",// Use env variable for security
 });
 
 const parser = new JsonOutputParser();
 
-const promptTemplate = PromptTemplate.fromTemplate(
-    `I have the following code:\n${code}\nI want you to correct the syntax and make the following changes:\n${changes}\nReturn the corrected code in JSON format.`
-);
+export async function getCorrectedCode(fileContent) {
+    const promptTemplate = PromptTemplate.fromTemplate(
+        `I have the following code: ${fileContent} nI want you to correct the syntax.Return the corrected code in JSON format.`
+    );
+    
 
-export async function getCorrectedCode() {
     try {
         const prompt = await promptTemplate.format({});
-        const response = await model.call([ { role: "user", content: prompt } ]);
+        const response = await model.call([{ role: "user", content: prompt }]);
         const correctedCode = await parser.parse(response.text); 
         return correctedCode; // Return the corrected code
     } catch (error) {
